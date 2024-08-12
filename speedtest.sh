4ec54159ae61c2768fdc02a88e19805dfce23b79
Chris Campbell <ccmpbll@users.noreply.github.com>
2024-08-12 14:32:24 -0400

    fixes for mqtt functions

--- speedtest.sh
+++ speedtest.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 
 mqttwithpass() {
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/ping/jitter -m $(jq -r '.ping.jitter' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/ping/latency -m $(jq -r '.ping.latency' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/ping/low -m $(jq -r '.ping.low' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/ping/high -m $(jq -r '.ping.high' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/download/bandwidth -m $(echo "scale=2; $(jq -r '.download.bandwidth' /tmp/speedtest-result)/125000" | bc)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/iqm -m $(jq -r '.download.latency.iqm' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/low -m $(jq -r '.download.latency.low' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/high -m $(jq -r '.download.latency.high' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/jitter -m $(jq -r '.download.latency.jitter' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/upload/bandwidth -m $(echo "scale=2; $(jq -r '.upload.bandwidth' /tmp/speedtest-result)/125000" | bc)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/iqm -m $(jq -r '.upload.latency.iqm' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/low -m $(jq -r '.upload.latency.low' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/high -m $(jq -r '.upload.latency.high' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/jitter -m $(jq -r '.upload.latency.jitter' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/packetloss -m $(jq -r '.packetLoss' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/isp -m $(jq -r '.isp' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/interface/externalip -m $(jq -r '.interface.externalIp' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/server/id -m $(jq -r '.server.id' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/server/name -m $(jq -r '.server.name' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/server/location -m $(jq -r '.server.location' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/server/country -m $(jq -r '.server.country' /tmp/speedtest-result)
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/server/ip -m $(jq -r '.server.ip' /tmp/speedtest-result)
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/ping/jitter -m "$(jq -r '.ping.jitter' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/ping/latency -m "$(jq -r '.ping.latency' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/ping/low -m "$(jq -r '.ping.low' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/ping/high -m "$(jq -r '.ping.high' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/download/bandwidth -m "$(echo "scale=2; $(jq -r '.download.bandwidth' /tmp/speedtest-result)/125000" | bc)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/iqm -m "$(jq -r '.download.latency.iqm' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/low -m "$(jq -r '.download.latency.low' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/high -m "$(jq -r '.download.latency.high' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/jitter -m "$(jq -r '.download.latency.jitter' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/upload/bandwidth -m "$(echo "scale=2; $(jq -r '.upload.bandwidth' /tmp/speedtest-result)/125000" | bc)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/iqm -m "$(jq -r '.upload.latency.iqm' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/low -m "$(jq -r '.upload.latency.low' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/high -m "$(jq -r '.upload.latency.high' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/jitter -m "$(jq -r '.upload.latency.jitter' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/packetloss -m "$(jq -r '.packetLoss' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/isp -m "$(jq -r '.isp' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/interface/externalip -m "$(jq -r '.interface.externalIp' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/server/id -m "$(jq -r '.server.id' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/server/name -m "$(jq -r '.server.name' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/server/location -m "$(jq -r '.server.location' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/server/country -m "$(jq -r '.server.country' /tmp/speedtest-result)"
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC/server/ip -m "$(jq -r '.server.ip' /tmp/speedtest-result)"
 }
 
 mqttnopass() {
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/ping/jitter -m $(jq -r '.ping.jitter' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/ping/latency -m $(jq -r '.ping.latency' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/ping/low -m $(jq -r '.ping.low' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/ping/high -m $(jq -r '.ping.high' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/download/bandwidth -m $(echo "scale=2; $(jq -r '.download.bandwidth' /tmp/speedtest-result)/125000" | bc)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/iqm -m $(jq -r '.download.latency.iqm' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/low -m $(jq -r '.download.latency.low' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/high -m $(jq -r '.download.latency.high' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/jitter -m $(jq -r '.download.latency.jitter' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/upload/bandwidth -m $(echo "scale=2; $(jq -r '.upload.bandwidth' /tmp/speedtest-result)/125000" | bc)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/iqm -m $(jq -r '.upload.latency.iqm' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/low -m $(jq -r '.upload.latency.low' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/high -m $(jq -r '.upload.latency.high' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/jitter -m $(jq -r '.upload.latency.jitter' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/packetloss -m $(jq -r '.packetLoss' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/isp -m $(jq -r '.isp' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/interface/externalip -m $(jq -r '.interface.externalIp' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/server/id -m $(jq -r '.server.id' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/server/name -m $(jq -r '.server.name' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/server/location -m $(jq -r '.server.location' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/server/country -m $(jq -r '.server.country' /tmp/speedtest-result)
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/server/ip -m $(jq -r '.server.ip' /tmp/speedtest-result)
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/ping/jitter -m "$(jq -r '.ping.jitter' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/ping/latency -m "$(jq -r '.ping.latency' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/ping/low -m "$(jq -r '.ping.low' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/ping/high -m "$(jq -r '.ping.high' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/download/bandwidth -m "$(echo "scale=2; $(jq -r '.download.bandwidth' /tmp/speedtest-result)/125000" | bc)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/iqm -m "$(jq -r '.download.latency.iqm' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/low -m "$(jq -r '.download.latency.low' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/high -m "$(jq -r '.download.latency.high' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/download/latency/jitter -m "$(jq -r '.download.latency.jitter' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/upload/bandwidth -m "$(echo "scale=2; $(jq -r '.upload.bandwidth' /tmp/speedtest-result)/125000" | bc)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/iqm -m "$(jq -r '.upload.latency.iqm' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/low -m "$(jq -r '.upload.latency.low' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/high -m "$(jq -r '.upload.latency.high' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/upload/latency/jitter -m "$(jq -r '.upload.latency.jitter' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/packetloss -m "$(jq -r '.packetLoss' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/isp -m "$(jq -r '.isp' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/interface/externalip -m "$(jq -r '.interface.externalIp' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/server/id -m "$(jq -r '.server.id' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/server/name -m "$(jq -r '.server.name' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/server/location -m "$(jq -r '.server.location' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/server/country -m "$(jq -r '.server.country' /tmp/speedtest-result)"
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC/server/ip -m "$(jq -r '.server.ip' /tmp/speedtest-result)"
 }
 
 if [ ! -f /first_start ]; then
