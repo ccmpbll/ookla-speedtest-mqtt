fa1166a3e6bc3c63e4d2a6fe8472328746d6cea9
Chris Campbell <ccmpbll@users.noreply.github.com>
2024-08-12 17:37:48 -0400

    fixed variable names...

--- speedtest.sh
+++ speedtest.sh
@@ -2,16 +2,16 @@
 
 #functions and variables
 mqttwithpass() {
-	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC -f $RESULTS
+	mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC -f $RESULTS_PATH
 }
 
 mqttnopass() {
-	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC -f $RESULTS
+	mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC -f $RESULTS_PATH
 }
 
 convertmbps() {
-	jq ".download.bandwidth = "$(echo "scale=2; $(jq -r '.download.bandwidth' $RESULTS)/125000" | bc)"" $RESULTS | sponge $RESULTS
-	jq ".upload.bandwidth = "$(echo "scale=2; $(jq -r '.upload.bandwidth' $RESULTS)/125000" | bc)"" $RESULTS | sponge $RESULTS
+	jq ".download.bandwidth = "$(echo "scale=2; $(jq -r '.download.bandwidth' $RESULTS_PATH)/125000" | bc)"" $RESULTS_PATH | sponge $RESULTS_PATH
+	jq ".upload.bandwidth = "$(echo "scale=2; $(jq -r '.upload.bandwidth' $RESULTS_PATH)/125000" | bc)"" $RESULTS_PATH | sponge $RESULTS_PATH
 }
 
 RESULTS_FORMAT="json-pretty"
@@ -22,18 +22,18 @@ LOG_DATE_FORMAT="$(date +%D_%T)"
 #check if container has been started before
 if [ ! -f $FIRST_START_PATH ]; then
 	touch $FIRST_START_PATH
-	touch $RESULTS
+	touch $RESULTS_PATH
 
     if [[ "${SERVER_ID}" ]]; then
 		echo "$LOG_DATE_FORMAT - Running Ookla speed test using specified server..."
-        speedtest --accept-license --accept-gdpr --server-id=$SERVER_ID --format=$RESULTS_FORMAT &> $RESULTS
+        speedtest --accept-license --accept-gdpr --server-id=$SERVER_ID --format=$RESULTS_FORMAT &> $RESULTS_PATH
     else
 		echo "$LOG_DATE_FORMAT - Running Ookla speed test..."
-        speedtest --accept-license --accept-gdpr --format=$RESULTS_FORMAT &> $RESULTS
+        speedtest --accept-license --accept-gdpr --format=$RESULTS_FORMAT &> $RESULTS_PATH
     fi
 
 	#since this is the first run, the license acceptance info shows up at the beginning of our output
-	sed -i '1,16d' $RESULTS
+	sed -i '1,16d' $RESULTS_PATH
 
 	#the default value from the speedtest is in bytes, here we convert that to Mbps
 	convertmbps
@@ -48,7 +48,7 @@ if [ ! -f $FIRST_START_PATH ]; then
 	fi
 
 	echo "$LOG_DATE_FORMAT - Cleaning up for the next run..."
-	rm $RESULTS
+	rm $RESULTS_PATH
 	echo "$LOG_DATE_FORMAT - First run is complete. License and GDPR have been accepted."
 	echo "$LOG_DATE_FORMAT - Sleeping for $SLEEP Seconds..."
 	sleep $SLEEP
@@ -56,14 +56,14 @@ else
 	#if container has been started before, stdout is fine, no license acceptance
 	while true
 	do
-		touch $RESULTS
+		touch $RESULTS_PATH
 
 		if [[ "${SERVER_ID}" ]]; then
 			echo "$LOG_DATE_FORMAT - Running Ookla speed test using specified server..."
-			speedtest --server-id=$SERVER_ID --format=$RESULTS_FORMAT &> $RESULTS
+			speedtest --server-id=$SERVER_ID --format=$RESULTS_FORMAT &> $RESULTS_PATH
 		else
 			echo "$LOG_DATE_FORMAT - Running Ookla speed test..."
-			speedtest --format=$RESULTS_FORMAT &> $RESULTS
+			speedtest --format=$RESULTS_FORMAT &> $RESULTS_PATH
 		fi
 
 		#the default value from the speedtest is in bytes, here we convert that to Mbps
@@ -78,7 +78,7 @@ else
 		fi
 
 		echo "$LOG_DATE_FORMAT - Cleaning up for the next run..."
-		rm $RESULTS
+		rm $RESULTS_PATH
 		echo "$LOG_DATE_FORMAT - Sleeping for $SLEEP Seconds..."
 		sleep $SLEEP
 	done
